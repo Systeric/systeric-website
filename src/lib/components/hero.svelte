eb66eb82aad1c3a3a6eb901328f6def335ef6d02
Raffi Verrel Alessandro <44971155+rvalessandro@users.noreply.github.com>
2024-02-24 01:10:17 +0700

    feat: styled home and services section

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -2,10 +2,10 @@
 	import teamwork from '$lib/assets/teamwork.jpg';
 </script>
 
-<div id="hero" class="py-16 sm:py-64 mx-auto max-w-screen-xl">
+<div id="hero" class="py-16 sm:py-32 mx-auto max-w-screen-xl flex flex-col items-center sm:gap-8">
 	<a
 		href="https://interwise.app"
-		class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-primary-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+		class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-8 text-sm text-gray-700 bg-primary-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
 	>
 		<span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span>
 		<span class="text-sm font-medium">Interwise is out! See what's new</span>
@@ -22,18 +22,31 @@
 		>
 	</a>
 
-	<!-- <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Digitizing Ideas into Solutions</h1> -->
-	<div class="sm:flex sm:justify-start sm:items-center gap-3">
+	<div class="flex sm:justify-start sm:items-center gap-3 flex-col-reverse sm:flex-row mb-8">
 		<div class="flex-1">
-			<h1 class="h1 font-extrabold text-secondary-500 mb-4">Digitizing Ideas into Solutions</h1>
-			<p class="mb-8 text-lg font-normal text-gray-700 lg:text-xl dark:text-gray-400">
+			<h1 class="h1 font-extrabold text-secondary-500 mb-6">Digitizing Ideas into Solutions</h1>
+			<p class="text-lg font-normal text-gray-600 lg:text-xl mb-6">
 				We integrate product management, design and software engineering to enable enterprises and
 				technology disruptors across the globe to thrive as modern digital businesses.
 			</p>
+			<button type="button" class="btn variant-filled bg-primary-500">
+				Contact us
+				<svg
+					class="ml-2 -mr-1 w-5 h-5"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+						clip-rule="evenodd"
+					></path></svg
+				>
+			</button>
 		</div>
 
-		<div class="flex-1 hidden sm:block">
-			<img src={teamwork} alt="Teamwork" class="mb-8" />
+		<div class="flex-1">
+			<img src={teamwork} alt="Teamwork" class="mb-4 sm:mb-0" />
 		</div>
 	</div>
 
@@ -115,15 +128,4 @@
 			</a>
 		</div>
 	</div>
-
-	<!-- <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Learn more
-                <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                Watch video
-            </a>  
-        </div> -->
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Hero from '$lib/components/hero.svelte';
+	import Services from '$lib/components/services.svelte';
 </script>
 
 <div class="container px-8 sm:px-16 w-full mx-auto">
 	<Hero />
+	<Services />
 </div>
