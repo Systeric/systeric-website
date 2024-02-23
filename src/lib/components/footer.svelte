1900aa39a18a1bbf1b8698c6be392124287e1c72
Raffi Verrel Alessandro <44971155+rvalessandro@users.noreply.github.com>
2024-02-24 02:35:55 +0700

    feat: improved footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,63 +2,28 @@
 	import icon from '$lib/assets/icon.webp';
 </script>
 
-<footer class="py-4 sm:py-6 dark:bg-gray-800">
+<footer class="py-4 sm:py-6 max-w-3xl mx-auto">
 	<div class="mx-auto max-w-screen-xl">
-		<div class="md:flex md:justify-between">
+		<div class="sm:flex md:justify-between">
+			<div class="text-gray-500 mb-8 sm:mb-0">
+				<p class="h4 mb-4"><strong class="text-gray-700">Systeric</strong></p>
+				<div>
+					<p>PT Systeric Teknologi Indonesia</p>
+					<p>2nd Floor Benoa Square</p>
+					<p>Jl. Bypass Ngurah Rai No. 21A</p>
+					<p>Kuta Badung - Bali Indonesia 80361</p>
+				</div>
+			</div>
 			<div>
+				<p class="h4 mb-4"><strong class="text-gray-700">Company</strong></p>
+
 				<button on:click={() => (window.location.href = '/')} class="flex items-center mb-8">
 					<img src={icon} alt="Systeric icon" class="w-10 mr-3" />
 					<strong class="text-2xl font-bold text-primary-500 -mt-1">systeric</strong>
 				</button>
 
-				<div class="text-gray-500 mb-2">
-					<p><strong class="text-gray-700">PT Systeric Teknologi Indonesia</strong></p>
-					<p>2nd Floor Benoa Square</p>
-					<p>Jl. Bypass Ngurah Rai No. 21A</p>
-					<p>Kuta Badung - Bali Indonesia 80361</p>
-				</div>
-
 				<a class="text-gray-800" href="mailto:commercial@systeric.com">commercial@systeric.com</a>
 			</div>
-			<!-- <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-				<div>
-					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-						Resources
-					</h2>
-					<ul class="text-gray-600 dark:text-gray-400">
-						<li class="mb-4">
-							<a href="https://flowbite.com" class="hover:underline">Flowbite</a>
-						</li>
-						<li>
-							<a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-						Follow us
-					</h2>
-					<ul class="text-gray-600 dark:text-gray-400">
-						<li class="mb-4">
-							<a href="https://github.com/themesberg/flowbite" class="hover:underline">Github</a>
-						</li>
-						<li>
-							<a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-					<ul class="text-gray-600 dark:text-gray-400">
-						<li class="mb-4">
-							<a href="#" class="hover:underline">Privacy Policy</a>
-						</li>
-						<li>
-							<a href="#" class="hover:underline">Terms &amp; Conditions</a>
-						</li>
-					</ul>
-				</div>
-			</div> -->
 		</div>
 		<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 		<div class="sm:flex sm:items-center sm:justify-between">
