eacbea6c01ee3cdd863643d51636cd2e15fb12af
Raffi Verrel Alessandro <44971155+rvalessandro@users.noreply.github.com>
2024-02-28 16:51:24 +0700

    feat: styled input according to the design language

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -8,40 +8,36 @@
 			We'll strategize, design, and build the solutions you need to achieve success.
 		</p>
 		<form action="#" class="space-y-8">
-			<div>
-				<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-					>Your email</label
-				>
+			<label for="email" class="label">
+				<span>Your email</span>
 				<input
 					type="email"
 					id="email"
-					class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+					class="input border-gray-300"
 					placeholder="name@systeric.com"
 					required
 				/>
-			</div>
-			<div>
-				<label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-					>Subject</label
-				>
+			</label>
+			<label for="subject" class="label">
+				<span>Subject</span>
 				<input
 					type="text"
 					id="subject"
-					class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+					class="input border-gray-300"
 					placeholder="Let us know how we can help you"
 					required
 				/>
-			</div>
+			</label>
 			<div class="sm:col-span-2">
-				<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-					>Your message</label
-				>
-				<textarea
-					id="message"
-					rows="6"
-					class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-					placeholder="Leave a comment..."
-				></textarea>
+				<label for="message" class="label">
+					<span>Your message</span>
+					<textarea
+						id="message"
+						rows="6"
+						class="input border-gray-300"
+						placeholder="Leave a comment..."
+					></textarea>
+				</label>
 			</div>
 			<button
 				type="submit"
