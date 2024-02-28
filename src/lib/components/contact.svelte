814a55ff0d579b5d04a7bbc9701163f7939aa65f
Raffi Verrel Alessandro <44971155+rvalessandro@users.noreply.github.com>
2024-02-28 17:06:15 +0700

    feat: added discord integration to contact form

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -1,3 +1,38 @@
+<script lang="ts">
+	interface IFormData {
+		email: string;
+		subject: string;
+		message: string;
+	}
+
+	let formData: IFormData = {
+		email: '',
+		subject: '',
+		message: ''
+	};
+
+	async function onSubmit() {
+		try {
+			const content = `Email: ${formData.email}\nSubject: ${formData.subject}\nMessage: ${formData.message}`;
+
+			await fetch(
+				'https://discord.com/api/webhooks/1212339071855366154/26mf1Ji-FEl6ZZXY8f7bUhLmzrWFmsgELTRpYiv_eJ6z70Ws43uiHxgBu2MUM7OXiMgR',
+				{
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						content
+					})
+				}
+			);
+
+			// TODO popup
+		} catch (error) {
+			// TODO popup
+		}
+	}
+</script>
+
 <section id="contact-us">
 	<div class="py-8 lg:py-16 mx-auto max-w-screen-md">
 		<h2 class="h2 mb-4 tracking-tight font-extrabold sm:text-center text-secondary-500">
@@ -7,7 +42,7 @@
 			Ready to transform your business with innovative solutions? Let's discuss your project goals.
 			We'll strategize, design, and build the solutions you need to achieve success.
 		</p>
-		<form action="#" class="space-y-8">
+		<form on:submit|preventDefault={onSubmit} class="space-y-8">
 			<label for="email" class="label">
 				<span>Your email</span>
 				<input
@@ -16,6 +51,7 @@
 					class="input border-gray-300"
 					placeholder="name@systeric.com"
 					required
+					bind:value={formData.email}
 				/>
 			</label>
 			<label for="subject" class="label">
@@ -26,6 +62,7 @@
 					class="input border-gray-300"
 					placeholder="Let us know how we can help you"
 					required
+					bind:value={formData.subject}
 				/>
 			</label>
 			<div class="sm:col-span-2">
@@ -36,6 +73,7 @@
 						rows="6"
 						class="input border-gray-300"
 						placeholder="Leave a comment..."
+						bind:value={formData.message}
 					></textarea>
 				</label>
 			</div>
