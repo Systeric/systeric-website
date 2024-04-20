f00c219b2ebe83e2e2cc1b8a8989e078603666ec
Raffi Verrel Alessandro <44971155+rvalessandro@users.noreply.github.com>
2024-04-20 19:21:20 +0700

    feat: updated icons

--- src/lib/components/services.svelte
+++ src/lib/components/services.svelte
@@ -1,5 +1,7 @@
 <script>
 	// import portrait from '$lib/assets/portrait.webp';
+
+	import IconWrapper from './atoms/iconWrapper.svelte';
 </script>
 
 <section class="flex items-center gap-12 flex-col lg:flex-row bg-surface-500 rounded-lg">
@@ -26,21 +28,18 @@
 		</div>
 		<div class="space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
 			<div>
-				<div
-					class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900"
-				>
+				<IconWrapper>
 					<svg
-						class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-						fill="currentColor"
-						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-							clip-rule="evenodd"
-						></path></svg
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-5 h-5"
 					>
-				</div>
+						<path
+							d="M11.25 5.337c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.036 1.007-1.875 2.25-1.875S15 2.34 15 3.375c0 .369-.128.713-.349 1.003-.215.283-.401.604-.401.959 0 .332.278.598.61.578 1.91-.114 3.79-.342 5.632-.676a.75.75 0 0 1 .878.645 49.17 49.17 0 0 1 .376 5.452.657.657 0 0 1-.66.664c-.354 0-.675-.186-.958-.401a1.647 1.647 0 0 0-1.003-.349c-1.035 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401.31 0 .557.262.534.571a48.774 48.774 0 0 1-.595 4.845.75.75 0 0 1-.61.61c-1.82.317-3.673.533-5.555.642a.58.58 0 0 1-.611-.581c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.035-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959a.641.641 0 0 1-.658.643 49.118 49.118 0 0 1-4.708-.36.75.75 0 0 1-.645-.878c.293-1.614.504-3.257.629-4.924A.53.53 0 0 0 5.337 15c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.036 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.369 0 .713.128 1.003.349.283.215.604.401.959.401a.656.656 0 0 0 .659-.663 47.703 47.703 0 0 0-.31-4.82.75.75 0 0 1 .83-.832c1.343.155 2.703.254 4.077.294a.64.64 0 0 0 .657-.642Z"
+						/>
+					</svg>
+				</IconWrapper>
 				<h3 class="mb-2 text-xl font-semibold dark:text-white">Product Strategy</h3>
 				<p class="text-gray-500 dark:text-gray-400">
 					<strong class="text-primary-500">Unsure where to take your product next?</strong> Unlock a
@@ -49,23 +48,18 @@
 				</p>
 			</div>
 			<div>
-				<div
-					class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900"
-				>
+				<IconWrapper>
 					<svg
-						class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-						fill="currentColor"
-						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
-						><path
-							d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"
-						></path><path
-							fill-rule="evenodd"
-							d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
-							clip-rule="evenodd"
-						></path></svg
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-5 h-5"
 					>
-				</div>
+						<path
+							d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z"
+						/>
+					</svg>
+				</IconWrapper>
 				<h3 class="mb-2 text-xl font-semibold dark:text-white">UI/UX Design</h3>
 				<p class="text-gray-500 dark:text-gray-400">
 					<strong class="text-primary-500">Want to create a product users love?</strong> Elevate your
@@ -74,19 +68,20 @@
 				</p>
 			</div>
 			<div>
-				<div
-					class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900"
-				>
+				<IconWrapper>
 					<svg
-						class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-						fill="currentColor"
-						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
-						><path
-							d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"
-						></path></svg
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-5 h-5"
 					>
-				</div>
+						<path
+							fill-rule="evenodd"
+							d="M14.447 3.026a.75.75 0 0 1 .527.921l-4.5 16.5a.75.75 0 0 1-1.448-.394l4.5-16.5a.75.75 0 0 1 .921-.527ZM16.72 6.22a.75.75 0 0 1 1.06 0l5.25 5.25a.75.75 0 0 1 0 1.06l-5.25 5.25a.75.75 0 1 1-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 0 1 0-1.06Zm-9.44 0a.75.75 0 0 1 0 1.06L2.56 12l4.72 4.72a.75.75 0 0 1-1.06 1.06L.97 12.53a.75.75 0 0 1 0-1.06l5.25-5.25a.75.75 0 0 1 1.06 0Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</IconWrapper>
 				<h3 class="mb-2 text-xl font-semibold dark:text-white">Software Development</h3>
 				<p class="text-gray-500 dark:text-gray-400">
 					<strong class="text-primary-500">Ready to build your dream software?</strong> Bring your vision
@@ -95,23 +90,23 @@
 				</p>
 			</div>
 			<div>
-				<div
-					class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900"
-				>
+				<IconWrapper>
 					<svg
-						class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-						fill="currentColor"
-						viewBox="0 0 20 20"
 						xmlns="http://www.w3.org/2000/svg"
-						><path
-							fill-rule="evenodd"
-							d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-							clip-rule="evenodd"
-						></path><path
-							d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"
-						></path></svg
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="w-5 h-5"
 					>
-				</div>
+						<path
+							d="M5.507 4.048A3 3 0 0 1 7.785 3h8.43a3 3 0 0 1 2.278 1.048l1.722 2.008A4.533 4.533 0 0 0 19.5 6h-15c-.243 0-.482.02-.715.056l1.722-2.008Z"
+						/>
+						<path
+							fill-rule="evenodd"
+							d="M1.5 10.5a3 3 0 0 1 3-3h15a3 3 0 1 1 0 6h-15a3 3 0 0 1-3-3Zm15 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm2.25.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM4.5 15a3 3 0 1 0 0 6h15a3 3 0 1 0 0-6h-15Zm11.25 3.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM19.5 18a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</IconWrapper>
 				<h3 class="mb-2 text-xl font-semibold dark:text-white">Infrastructure Migration</h3>
 				<p class="text-gray-500 dark:text-gray-400">
 					<strong class="text-primary-500"
